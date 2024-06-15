34007d18a31cff84b0ecce9ed1ae8e76eb55ca64
Wellington <wellington201197@gmail.com>
2024-06-15 13:03:55 -0300

    melhorias frontend 2

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,8 @@ import Habilidades from "./components/Habilidades.vue"
 import Experiencias from "./components/Experiencias.vue"
 import Contato from "./components/Contato.vue"
 import FooterMenu from "./components/FooterMenu.vue"
+import throttle from 'lodash/throttle';
+
 export default {
 	name: "App",
 	components: {
@@ -43,50 +45,54 @@ export default {
 			theme: "dark"
 		}
 	},
-	mounted(){
-		document.addEventListener('scroll', this.changeNavbar);
+	mounted() {
+		// Adiciona o evento de rolagem com throttling
+		this.throttledChangeNavbar = throttle(this.changeNavbar, 100);
+		document.addEventListener('scroll', this.throttledChangeNavbar);
 
-		
 		setTimeout(() => {
-			let element = document.getElementById('inicio')
-			element.classList.add('content-page-show')
-			let animation_el = element.querySelector('.animation_content')
-			animation_el.classList.add('animate__animated', animation_el.getAttribute('data-animate'))
-			window.dispatchEvent(new Event('init'))
-			document.getElementsByClassName('dg')[0].remove()
+			let element = document.getElementById('inicio');
+			element.classList.add('content-page-show');
+			let animation_el = element.querySelector('.animation_content');
+			animation_el.classList.add('animate__animated', animation_el.getAttribute('data-animate'));
+			window.dispatchEvent(new Event('init'));
+			document.getElementsByClassName('dg')[0].remove();
 		}, 100);
-		
 	},
-	methods:{
-		changeTheme(theme){
-			this.theme = theme
+	beforeDestroy() {
+		// Remove o evento de rolagem ao destruir o componente
+		document.removeEventListener('scroll', this.throttledChangeNavbar);
+	},
+	methods: {
+		changeTheme(theme) {
+			this.theme = theme;
 		},
-		changeNavbar(){
-			let elementsArray = document.querySelectorAll(".content-page-animation");
-			for(let i=0; i < elementsArray.length; i++){
+		changeNavbar() {
+			let elementsArray = document.querySelectorAll('.content-page-animation');
+			for (let i = 0; i < elementsArray.length; i++) {
 				var rect = elementsArray[i].getBoundingClientRect();
 				var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-				if(!(rect.bottom < 0 || rect.top - viewHeight >= 0)){
-					if(elementsArray[i].className.indexOf('content-page-show') == -1){
-						let animation_el = elementsArray[i].querySelector('.animation_content')
-						if(animation_el){
-							animation_el.classList.add('animate__animated', animation_el.getAttribute('data-animate'))
+				if (!(rect.bottom < 0 || rect.top - viewHeight >= 0)) {
+					if (elementsArray[i].className.indexOf('content-page-show') === -1) {
+						let animation_el = elementsArray[i].querySelector('.animation_content');
+						if (animation_el) {
+							animation_el.classList.add('animate__animated', animation_el.getAttribute('data-animate'));
 						}
-						elementsArray[i].classList.add('content-page-show')
+						elementsArray[i].classList.add('content-page-show');
 					}
-				}else{
-					if(elementsArray[i].className.indexOf('content-page-show') != -1){
-						let animation_el = elementsArray[i].querySelector('.animation_content')
-						if(animation_el){
-							animation_el.classList.remove('animate__animated', animation_el.getAttribute('data-animate'))
+				} else {
+					if (elementsArray[i].className.indexOf('content-page-show') !== -1) {
+						let animation_el = elementsArray[i].querySelector('.animation_content');
+						if (animation_el) {
+							animation_el.classList.remove('animate__animated', animation_el.getAttribute('data-animate'));
 						}
-						elementsArray[i].classList.remove('content-page-show')
+						elementsArray[i].classList.remove('content-page-show');
 					}
 				}
 			}
 		},
-		scrollToElement(href){
-			let element = document.querySelector(href)
+		scrollToElement(href) {
+			let element = document.querySelector(href);
 			element.scrollIntoView();
 		}
 	}
